1cce4281cd2ae7d80aae90fcb3233ab5180d1afe
Drama <abandoneddrama@foxmail.com>
2017-05-08 16:19:25 +0800

    控制新闻详情页图片最大宽度

--- src/components/newsDetailsHtml.vue
+++ src/components/newsDetailsHtml.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="newsDetailsHtml">
     <h1 style="text-align: center;margin-bottom: 30px;color: #ccc;">{{ details.title }}</h1>
-    <div v-html="details.details"></div>
+    <div class="newbody" v-html="details.details"></div>
   </div>
 </template>
+
+<style lang="less">
+  #newsDetailsHtml{
+    .newbody{
+      img{
+        max-width: 80%;
+      }
+    }
+  }
+</style>
 
 <script>
   export default{
